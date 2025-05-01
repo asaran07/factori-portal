44868f5dfb7127dfd0299ba0fbf947139d5fddcf
asaran07 <saran.arsh07@icloud.com>
2025-05-01 13:00:25 -0700

    Add detailed comments to the first four relations in the main db schema

--- sql/dev/01_schema.sql
+++ sql/dev/01_schema.sql
@@ -1,23 +1,33 @@
+-- like a "catalog" of all items that can exist in a factory/inventory.
 CREATE TABLE IF NOT EXISTS items (
     item_id SERIAL PRIMARY KEY,
-    item_name VARCHAR(50),
+    item_name VARCHAR(50) NOT NULL,
     description VARCHAR(50)
 );
 
-
+-- table for the general category of a unit an attribute can have.
+-- for example, distance, weight, time etc.
 CREATE TABLE IF NOT EXISTS unit_types (
     type_id SERIAL PRIMARY KEY,
     type_name VARCHAR(30) NOT NULL
 );
 
+-- this table containts the specific units like meters, inches, kilograms, etc.
 CREATE TABLE IF NOT EXISTS unit_definitions (
-    unit_id SERIAL PRIMARY KEY, 
+    unit_id SERIAL PRIMARY KEY,
     unit_name VARCHAR(20) NOT NULL,
     type_id INT,
-
+    -- the foreign key references the `unit_types` table because
+    -- the tuples in this table need to fall under a unit category.
+    -- for example, kilograms is a `unit_definition` of type 'weight'.
     FOREIGN KEY (type_id) REFERENCES unit_types (type_id)
 );
 
+-- this is the actual table the stores the different attributes that
+-- can be assigned to different items. for example, the manager
+-- could create an attribute of name 'width', which is a 'numeric'
+-- value, and of type 'distance'. which is why it's foreign key
+-- references the `unit_types` table.
 CREATE TABLE IF NOT EXISTS attribute_definitions (
     definition_id SERIAL PRIMARY KEY,
     attribute_name VARCHAR(50) NOT NULL,
