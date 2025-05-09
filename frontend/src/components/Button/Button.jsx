9bcf488719c689f14b1ab2276ece583dc233712c
asaran07 <saran.arsh07@icloud.com>
2025-05-09 16:04:59 -0700

    feat(react/button): modify button to be an actual button with onClick and props parameters

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -1,5 +1,9 @@
 import styles from "./Button.module.css";
 
-export default function Button({ children }) {
-  return <div className={styles.button}>{children}</div>;
+export default function Button({ children, onClick, ...props }) {
+  return (
+    <button className={styles.button} onClick={onClick} {...props}>
+      {children}
+    </button>
+  );
 }
